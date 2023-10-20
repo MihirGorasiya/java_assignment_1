d2723c0e0af55f761a7aa8a6df48b7c54ed22bbb
Mihir Gorasiya <mihirgorasiya@gmail.com>
2023-10-20 18:43:15 -0400

    Coordinate take from user is implemented

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,28 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         float[][] coordinates = new float[3][2];
 
+        var main = new Main();
+
+        coordinates = main.getCoordinates(scanner);
+
+
+
+
+
+    }
+
+
+    public float[][] getCoordinates(Scanner scan) {
+        float[][] coordinates = new float[3][2];
+
         for (int i = 0; i < 3; i++) {
             System.out.println("Construction of a new point");
             System.out.print("Please enter x: ");
-            coordinates[i][0] = scanner.nextFloat();
+            coordinates[i][0] = scan.nextFloat();
             System.out.print("Please enter y: ");
-            coordinates[i][1] = scanner.nextFloat();
+            coordinates[i][1] = scan.nextFloat();
         }
 
-
-    }
-
-    public float[][] getCoordinates() {
-        float[][] coordinates = new float[3][2];
         return coordinates;
     }
-
 }
