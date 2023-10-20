92345fad0782075ff13a6d5c53f9abdb3cc4b7fc
Mihir Gorasiya <mihirgorasiya@gmail.com>
2023-10-20 18:36:50 -0400

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        float[][] coordinates = new float[3][2];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Construction of a new point");
+            System.out.print("Please enter x: ");
+            coordinates[i][0] = scanner.nextFloat();
+            System.out.print("Please enter y: ");
+            coordinates[i][1] = scanner.nextFloat();
+        }
+
+
     }
+
+    public float[][] getCoordinates() {
+        float[][] coordinates = new float[3][2];
+        return coordinates;
+    }
+
 }
